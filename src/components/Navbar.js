2c6e29eacebd3a3eb4858fcd812fd1dc0a9c78fc
Kowsik002 <kowsiknarumanchi577@gmail.com>
2024-11-08 19:29:03 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,24 @@ const Navbar = () => {
     <nav className="bg-gray-700 text-white fixed w-full top-0 left-0 shadow-md z-10">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         <h1 className="text-2xl font-semibold">Kowsik's Portfolio</h1>
+        <div className="sm:hidden">
+          <button onClick={toggleMenu} className="text-white">
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
 
         <ul
           className={`sm:flex ${
